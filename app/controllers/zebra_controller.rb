2446318c434ba3e2f8caa6c1041421a176cad2d1
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-09-26 21:58:32 +0000

    quicksave

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -22,7 +22,9 @@ class ZebraController <ApplicationController
     render({ template: "dice_templates/squareroot"})
   end
   def cow
+    @the_num = params.fetch("users_number").to_f
 
+    @the_result = Math.sqrt(@the_num).to_f
     render({ template: "dice_templates/squareroot_results"})
   end
 
@@ -31,7 +33,14 @@ class ZebraController <ApplicationController
     render({ template: "dice_templates/payment"})
   end
   def dog
+    @principal = params.fetch("users_principal").to_fs(:currency)
+    @apr = params.fetch("users_apr").to_fs(:percentage) / 100 /12
+    @years = params.fetch("users_year").to_f * 12
+  
+    @top = @apr * (@principal)
+    @bottom = 1 - ((1 + @apr) ** (-1 * @years))  
 
+    @payment = @top/@bottom
     render({ template: "dice_templates/payment_results"})
   end
 
@@ -40,7 +49,10 @@ class ZebraController <ApplicationController
     render({ template: "dice_templates/random"})
   end
   def shrew
-
+    @min = params.fetch("users_min").to_f
+    @max = params.fetch("users_max").to_f
+  
+    @random_number = rand(@min..@max)
     render({ template: "dice_templates/random_results"})
   end
 end
